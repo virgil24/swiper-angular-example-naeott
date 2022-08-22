d9ef44cfa2af373608e27bf4b0fa8505bea8f099
virgil24 <kwiatkowski.virgil@gmail.com>
2022-08-22 16:23:03 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectorRef, Component, ViewChild } from "@angular/core";
+
+
+@Component({
+  selector: "app-swiper-example",
+  
+  templateUrl: "app.component.html"
+})
+export class AppComponent {
+  
+
+
+ 
+}
